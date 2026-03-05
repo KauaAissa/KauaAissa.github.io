0929492e298f6b508601d511dc9d9cffe82c4bb0
KauaAissa <kaua.aissa.dev@gmail.com>
2026-03-05 20:48:16 -0300

    FEAT: Adding photos to project

--- js/scripts.js
+++ js/scripts.js
@@ -63,3 +63,25 @@ timeline
     duration: 1,
     ease: "power2.out",
   });
+
+  const swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    640: { slidesPerView: 2 },
+    1024: { slidesPerView: 3 },
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+});
